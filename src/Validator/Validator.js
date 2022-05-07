df1bfcdd763108c0b32ee6a3490147c9ef0f40d6
Sjim de Munck <s.munck@hoffelijk.nl>
2022-05-07 08:37:58 +0200

    Remove confusing return statement

--- src/Validator/Validator.js
+++ src/Validator/Validator.js
@@ -2,7 +2,7 @@ const validator = (schema, obj) => {
     for (const [key, value] of Object.entries(schema)) {
         const objValue = obj[key];
         if (value !== typeof objValue || Array.isArray(objValue)) {
-            return !!(value === 'array' && Array.isArray(objValue));
+            return (value === 'array' && Array.isArray(objValue));
         }
     }
 
